a6720f95ef035d71f2bbd0ee18d5442f08c8e825
sergiosob <sobrito@gmail.com>
2021-07-23 18:15:33 -0300

    Projeto Contactura na versão 9

---
+++ contactura/src/app/form-contatos/form-contatos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-contatos',
+  templateUrl: './form-contatos.component.html',
+  styleUrls: ['./form-contatos.component.css']
+})
+export class FormContatosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ contactura/src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+  });
+
+  //playAudio(){
+  //  let audio = new Audio();
+  //  audio.src = "../../../assets/audio/Vivaldi4seasons.mp3";
+  // audio.load();
+  // audio.play();
+  //}
+ 
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    //this.playAudio();
+  }
+
+  login(){
+    if (this.loginForm.valid) {
+      localStorage.setItem('token', 'fuctura');
+      this.router.navigate(['/lista-contatos']);
+   
+    }
+  }
+}
